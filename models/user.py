334150744cfd54dbbd5cda131ae759a4bb6018aa
MrSamy91 <samy.ajouid@gmail.com>
2024-03-06 15:48:00 +0100

    adding some code into user.py

--- models/user.py
+++ models/user.py
@@ -1,15 +1,20 @@
 from base_model import BaseModel
 
 class User(BaseModel):
-    def __init__(self, username, email, *args, **kwargs):
+    def __init__(self, username, email, full_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.username = username
         self.email = email
+        self.full_name = full_name
+
+    def greet(self):
+        return f"Hello, {self.full_name}!"
 
     def to_dict(self):
         user_dict = super().to_dict()
         user_dict.update({
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'full_name': self.full_name
         })
         return user_dict
