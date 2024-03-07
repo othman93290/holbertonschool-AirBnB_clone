8340f734b8d76142d361538ae71e1a04918591ae
othman93290 <7566@holbertonstudents.com>
2024-03-07 20:12:57 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,20 +1,20 @@
+#!/usr/bin/python3
+"""Module for User class"""
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-    def __init__(self, username, email, full_name, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.username = username
-        self.email = email
-        self.full_name = full_name
+    """
+    Description:
+        Class that defines user attributes.
 
-    def greet(self):
-        return f"Hello, {self.full_name}!"
-
-    def to_dict(self):
-        user_dict = super().to_dict()
-        user_dict.update({
-            'username': self.username,
-            'email': self.email,
-            'full_name': self.full_name
-        })
-        return user_dict
+    Attributes:
+        email: user's email
+        password: user's password
+        first_name: user's first name
+        last_name: user's last name
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
